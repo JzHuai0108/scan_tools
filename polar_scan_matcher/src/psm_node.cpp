8dd44beb06b3cff952c2620d0649d09ee3e9bcae
Jianzhu Huai <huai.3@osu.edu>
2023-06-13 12:36:33 +0800

    check multiple publishers and subscribers in one thread

--- polar_scan_matcher/src/psm_node.cpp
+++ polar_scan_matcher/src/psm_node.cpp
@@ -29,12 +29,49 @@
 */
 
 #include "polar_scan_matcher/psm_node.h"
+#include "polar_scan_matcher/talker.h"
+#include "polar_scan_matcher/listener.h"
 
 int main (int argc, char** argv)
 {
   ros::init(argc, argv, "PolarScanMatching Node");
+  Talker talker;
+  Listener listener;
   PSMNode psmNode;
-  ros::spin();
+  ros::Rate loop_rate(10);
+  int count = 0;
+  while (ros::ok()) {
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+// %EndTag(FILL_MESSAGE)%
+
+// %Tag(ROSCONSOLE)%
+    ROS_INFO("%s", msg.data.c_str());
+// %EndTag(ROSCONSOLE)%
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+// %Tag(PUBLISH)%
+    talker.publish(msg);
+// %EndTag(PUBLISH)%
+
+// %Tag(SPINONCE)%
+    ros::spinOnce();
+// %EndTag(SPINONCE)%
+
+// %Tag(RATE_SLEEP)%
+    loop_rate.sleep();
+// %EndTag(RATE_SLEEP)%
+    ++count;
+  }
+  return 0;
+  // ros::spin();
 }
 
 PSMNode::PSMNode()
